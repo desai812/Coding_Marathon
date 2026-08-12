7c0724fe3764cb306cb283069b2ef716330e61a6
Gauri Desai <desaigauri812@gmail.com>
2026-08-12 22:09:48 +0530

    Create 1700.java

---
+++ 1700.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int countStudents(int[] students, int[] sandwiches) {
+
+        int count0 = 0;
+        int count1 = 0;
+
+        // Count students who want 0 and 1
+        for (int student : students) {
+            if (student == 0)
+                count0++;
+            else
+                count1++;
+        }
+
+        // Check sandwiches
+        for (int sandwich : sandwiches) {
+
+            if (sandwich == 0) {
+                if (count0 == 0)
+                    break;
+
+                count0--;
+            } 
+            else {
+                if (count1 == 0)
+                    break;
+
+                count1--;
+            }
+        }
+
+        return count0 + count1;
+    }
+}
